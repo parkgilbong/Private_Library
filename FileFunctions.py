4ea781aaa981ed62ee44eb786043cdebbaa25e4f
Yong Gyu Kim <parkgilbong@gmail.com>
2024-12-20 11:29:01 +0900

    modified by YG

--- FileFunctions.py
+++ FileFunctions.py
@@ -1,10 +1,16 @@
-def Set_WD(base_folder, *args):
+from pathlib import Path
+import os
+
+def Set_WD(base_folder: str, *args: str) -> str:
     """
-    주어진 base folder와 추가 폴더 이름들을 사용하여 경로를 생성하고, 해당 경로를 working directory로 설정합니다. 
+    Creates a path using the given base folder and additional folder names, and sets it as the working directory. 
 
     Parameters:
-    - base_folder: str. the base path for setting the working directory 
-    - *args:     : str. the name of folder that the user wants to make under the base folder
+    - base_folder (str): The base path for setting the working directory. This should be an existing directory.
+    - *args (str): Additional folder names to be appended to the base folder. These can be any number of strings representing folder names.
+
+    Returns:
+    - str: The full path of the new working directory.
 
     Example:
         base_folder = 'C:/Users/YGKim_IBS/Documents' 
@@ -14,66 +20,66 @@ def Set_WD(base_folder, *args):
         The working directory is set to C:/Users/YGKim_IBS/Documents\test1\test2.
     """
 
-    import os
-
-    #전체 경로 생성
     full_path = os.path.join(base_folder, *args)
 
-    if not os.path.exists(full_path): # If the path does not exist, create the directory 
-       os.makedirs(full_path)
-       os.chdir(full_path)
-       print(f"Directory {full_path} created.")
-       print(f"The working directory is set to {full_path}.") 
-    else:
-         os.chdir(full_path)
-         print(f"Directory {full_path} already exists.")
-         print(f"The working directory is set to {full_path}.")
+    try:
+        full_path.mkdir(parents=True, exist_ok=True)
+        os.chdir(full_path)
+        if full_path.exists():
+            print(f"Directory {full_path} already exists.")
+        else:
+            print(f"Directory {full_path} created.")
+        print(f"The working directory is set to {full_path}.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    return str(full_path)
 
 ################################################################################################################
 
-def Grab_files_in_folder(folder_path, ext=""):
+def Grab_files_in_folder(folder_path: str, ext: str="") -> list:
     """
-    Return the paths of files with extension *ext* present in *foler*.
+    Returns the paths of files with the specified extension present in the given folder.
 
     Parameters:
-    - folder: str. the path of the directory where you want to start the search
-    - ext   : str. the file extension you are looking for (e.g., '.txt' for text files, '.py' for Python files) 
+    - folder_path (str): The path of the directory where you want to start the search. This should be an existing directory.
+    - ext (str): The file extension you are looking for (e.g., '.txt' for text files, '.py' for Python files). If no extension is provided, all files in the folder will be returned.
+
+    Returns:
+    - list: A list of file paths that match the specified extension within the given folder.
+
+    Example:
+        base = "C:/Users/YGKim_IBS/Documents/Github/Jupyter_notebook/FiberPhotometry"
+        Grab_files_in_folder(base, ext='.ipynb')
+        =========================================================================
+        This will return a list of all Jupyter Notebook files (.ipynb) in the specified folder.
+    """
+
+    folder = Path(folder_path)
+    if not folder.is_dir():
+        raise ValueError(f"The provided path {folder_path} is not a valid directory.")
+
+    return [str(file) for file in folder.glob(f'*{ext}') if file.is_file()]
+
+################################################################################################################
+
+def Grab_files_in_all_subfolder(folder_path: str, ext: str="") -> list:
+    """
+    Return the paths of files with the specified extension present in the given folder including *all sub-foler*.
+
+    Parameters:
+    - folder (str): The path of the directory where you want to start the search
+    - ext   : The file extension you are looking for (e.g., '.txt' for text files, '.py' for Python files) 
     
     Example:
     base = "C:/Users/YGKim_IBS/Documents/Github/Jupyter_notebook/FiberPhotometry"
     Grab_files_in_folder(base, ext='.ipynb')
     """
-
-    import os
-
-    list = []
-
-    for file in os.listdir(folder_path):
-        if file.endswith(ext):
-            list.append(os.path.join(folder_path, file))
-
-    return(list)
-
-################################################################################################################
-
-def Grab_files_in_all_subfolder(folder_path, ext=""):
-    """
-    Return the paths of files with extension *ext* present in *all sub-foler*.
-
-    Parameters:
-    - folder: str. the path of the directory where you want to start the search
-    - ext   : str. the file extension you are looking for (e.g., '.txt' for text files, '.py' for Python files) 
+    folder = Path(folder_path)
+    if not folder.is_dir():
+        raise ValueError(f"The provided path {folder_path} is not a valid directory.")
     
-    Example:
-    base = "C:/Users/YGKim_IBS/Documents/Github/Jupyter_notebook/FiberPhotometry"
-    Grab_files_in_folder(base, ext='.ipynb')
-    """
-
-    import os
-
-    list = [os.path.join(root, name) 
+    return[os.path.join(root, name) 
             for root, dirs, files in os.walk(folder_path)
             for name in files 
             if name.endswith(ext)]
-
-    return(list)
